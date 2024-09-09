cc314b96139708156f9a0d696f82d458e8de6748
Yann <yanngames68@gmail.com>
2024-09-09 17:55:23 -0300

    Memory database configuration for Integration Tests

--- tests/WebApi.Test/Users/Register/RegisterUserTests.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTests.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace WebApi.Test.Users.Register;
-public class RegisterUserTests : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTests : IClassFixture<CustomWebApplicationFactory>
 {
+    private const string METHOD = "api/User";
+
+    private readonly HttpClient _httpClient;
+    public RegisterUserTests(CustomWebApplicationFactory webApplicationFactory)
+    {
+        _httpClient = webApplicationFactory.CreateClient();   
+    }
+
     [Fact]
     public async Task Sucess()
     {
+        var request = RequestRegisterUserJsonBuilder.Build();
 
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.Created);
     }
 }
